fab6e93dd2dd1eaf8b60004a5d61293bc10be60a
husaam1 <82023622+husaam1@users.noreply.github.com>
2022-02-11 18:52:46 +0300

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -62,8 +62,7 @@ async def fetch_info(replied_user, event):
     caption += f"<b>• 𝗡𝗮𝗺𝗲 𖦹</b> {first_name} {last_name}\n"
     caption += f"<b>• 𝗨𝘀𝗲𝗿 𖦹</b> {username}\n"
     caption += f"<b>• 𝗜𝗱 𖦹</b> <code>{user_id}</code>\n"
-    caption += f"<b>• 𝗕𝗶𝗼
-𖦹</b> {user_bio}\n"
+    caption += f"<b>• 𝗕𝗶𝗼 𖦹</b> {user_bio}\n"
 ➖
     return photo, caption
 
