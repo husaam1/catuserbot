1be058cc212dea6ec0bdefd9b0128956f1598838
husaam1 <82023622+husaam1@users.noreply.github.com>
2022-02-11 17:44:37 +0300

    Update secret.py

--- userbot/assistant/secret.py
+++ userbot/assistant/secret.py
@@ -20,9 +20,9 @@ async def on_plug_in_callback_query_handler(event):
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
-                reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
+                reply_pop_up_alert = "شكلك تبينا نزعل من بعض ؟ عيب هالحركات"
         except KeyError:
             reply_pop_up_alert = "This message no longer exists in catub server"
     else:
-        reply_pop_up_alert = "This message no longer exists "
+        reply_pop_up_alert = "الهمسة لم تعد متاحة ♪ "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
